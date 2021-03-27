e6bc7f9a3680d9823a79bbc773ff01e8952cfe97
ovasquezbrito <ovasquezcyt@gmail.com>
2021-03-27 17:05:21 -0400

    Perfil de usuario

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from validate_email import validate_email
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextField 
 from wtforms.validators import (DataRequired, Length, EqualTo, Email, ValidationError)
@@ -54,3 +56,35 @@ class LoginForm(FlaskForm):
     
     submit = SubmitField('Iniciar sesión')
 
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        'Username',
+        [
+            DataRequired(message='Ingrese un nombre de usuario'), 
+            Length(min="2", max="20")
+        ]
+    )
+    email = TextField(
+        'Email',
+         [
+             DataRequired(message='Ingrese un correo'), 
+             validate_email
+        ]
+    )
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is token. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That username is token. Please choose a different one.')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -39,7 +42,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created! You are now able to login', 'success')
-        return redirect(url_for('login'))
+        return redirect(url_for('home'))
     
     return render_template('register.html', title='Register', form=form)
 
@@ -66,8 +69,41 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/account')
+def save_picture(form_picture):
+    randon_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = randon_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
+    form = UpdateAccountForm()
+    if form.is_submitted():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='images/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route('/post/new')
+@login_required
+def new_post():
+    return render_template('create_post.html', title='New Post')
